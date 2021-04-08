4b6be9cf177f00c576365041eadc3c70a1b74bca
Nking217 <38316404+Nking217@users.noreply.github.com>
2021-04-08 14:10:59 +0300

    A little code update

--- hydroponic/notifications.ino
+++ hydroponic/notifications.ino
@@ -9,11 +9,11 @@ void notifyPumpError(){
 }
 
 void notifyDrainageError(){
-  
+  Serial.print("Drainage Error, Please cheekc your drainage fosset.");
 }
 
 void notifyLowBatteryWarning(float voltage){
-  
+  Serial.print("Low battery levle. Please charge your battery.");
 }
 
 void notifyLowBatteryError(float voltage){
@@ -21,5 +21,5 @@ void notifyLowBatteryError(float voltage){
 }
 
 void showBatteryPrecent(byte percent){
-  
+  Serial.print("Battery percemt: ", percent);
 }
